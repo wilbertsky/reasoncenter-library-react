0b5bfa60a7f652c25571b9810740ec26d2523e4c
Will Scharff <willscharff@icloud.com>
2025-08-17 11:10:51 -0700

    Adds imageCheckApi to check if the open library image URL is a 404 or valid.

---
+++ src/Api/imageCheckApi.js
@@ -0,0 +1,16 @@
+export default async function checkUrlFor404(url) {
+  try {
+    const response = await fetch(url, { method: 'HEAD' }); // Using HEAD method for efficiency
+    const placeHolder = "https://placehold.co/200x300/EEE/31343C?font=lato&text=Image%20Unavailable";
+
+    if (response.status === 404) {
+      return placeHolder;
+    } else if (response.ok) { // Status code 200-299
+      return url;
+    } else {
+      return placeHolder;
+    }
+  } catch (error) {
+    return "https://placehold.co/200x300/EEE/31343C?font=lato&text=image%20is%20unavailable"; // Treat network errors as non-existence for this purpose
+  }
+}
